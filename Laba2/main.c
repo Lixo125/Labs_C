3664e555e1499203b828302ffae40a8e0c5d170e
Lixo125 <lixo_125@mail.ru>
2023-03-04 15:58:09 +1000

    Laba2

--- Laba2/main.c
+++ Laba2/main.c
@@ -3,19 +3,22 @@
 
 int main()
 {
-    int i, j;
-    double glavD, pobD;
-    double arr[3][3];
-    printf("Zadayte elementi massiva(double) 3x3:\n");
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 3; j++)
+    int i, j, r, n;
+    double glavD=0, pobD=0;
+    printf("Zadayte razmer massiva:\n");
+    scanf("%d", &n);
+    double arr[n][n];
+    double res[n][n];
+    printf("Zadayte elementi massiva(double):\n");
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
             scanf("%lf", &arr[i][j]);
 
     printf("\n");
 
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < n; i++)
     {
-        for (j = 0; j < 3; j++)
+        for (j = 0; j < n; j++)
         {
             printf("%2g\t\t", arr[i][j]);
         }
@@ -24,43 +27,27 @@ int main()
 
     printf("\n");
 
-    glavD = arr[0][0] + arr[1][1] + arr[2][2];
-    pobD = arr[0][2] + arr[1][1] + arr[2][0];
+    for (i = 0; i < n; i++)
+    {
+            glavD += arr[i][i];
+            pobD += arr[n-1-i][i];
+    }
+
 
     printf("Sum - Main dig: %g\nSum - Poboch dig: %g\n", glavD, pobD);
 
-    int arr1[2][2];
-
-    printf("\nZadayte elementi massiva(int) 2x2:\n");
-
-    for (i = 0; i < 2; i++)
-        for (j = 0; j < 2; j++)
-            scanf("%d", &arr1[i][j]);
-
-    printf("\n");
-
-    for (i = 0; i < 2; i++)
-    {
-        for (j = 0; j < 2; j++)
-        {
-            printf("%d\t", arr1[i][j]);
-        }
-        printf("\n");
-    }
-
     printf("\nKvadrat matrici:\n");
 
-    int arr2[2][2];
-    arr2[0][0] = arr1[0][0]*arr1[0][0] + arr1[0][1]*arr1[1][0];
-    arr2[0][1] = arr1[0][0]*arr1[0][1] + arr1[0][1]*arr1[1][1];
-    arr2[1][0] = arr1[1][0]*arr1[0][0] + arr1[1][1]*arr1[1][0];
-    arr2[1][1] = arr1[1][0]*arr1[0][1] + arr1[1][1]*arr1[1][1];
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            for (r = 0; r < n; r++)
+                res[i][j] += arr[i][r] * arr[r][j];
 
-    for (i = 0; i < 2; i++)
+    for (i = 0; i < n; i++)
     {
-        for (j = 0; j < 2; j++)
+        for (j = 0; j < n; j++)
         {
-            printf("%d\t", arr2[i][j]);
+            printf("%lf\t", res[i][j]);
         }
         printf("\n");
     }
