1b75d50e01e6c2fda52bcfdcd694461f9fe876c2
Lixo125 <lixo_125@mail.ru>
2023-03-18 22:51:32 +1000

    гоу

--- Laba6/main.c
+++ Laba6/main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
+#include <locale.h>
 
 int main() {
+    setlocale(LC_ALL, "RUS");
+
     FILE *input_file = fopen("input.txt", "r");
     FILE *output_file = fopen("output.txt", "w");
     char last_name[50], first_name[50], middle_name[50];
     int year;
 
     if (input_file == NULL) {
-        printf("Error opening input file.\n");
+        printf("Ошибка при открытии входного файла.\n");
         return 1;
     }
 
     while (fscanf(input_file, "%s %s %s %d", last_name, first_name, middle_name, &year) == 4) {
-        if (year > 1980) {
+        if (year > 1980 && last_name[0] == 'С') {
+            printf("%s %s %s %d\n", last_name, first_name, middle_name, year);
             fprintf(output_file, "%s %s %s %d\n", last_name, first_name, middle_name, year);
         }
     }
