2387d42f23dcc1a1af01f01dc9e29c70770df292
Lixo125 <lixo_125@mail.ru>
2023-03-18 15:51:55 +1000

    д

--- Laba6/main.c
+++ Laba6/main.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    for (int i = 0; i < 10; i++)
-    {
-        printf("%d\t", i+1);
+int main() {
+    FILE *input_file = fopen("input.txt", "r");
+    FILE *output_file = fopen("output.txt", "w");
+    char last_name[50], first_name[50], middle_name[50];
+    int year;
+
+    if (input_file == NULL) {
+        printf("Error opening input file.\n");
+        return 1;
     }
+
+    while (fscanf(input_file, "%s %s %s %d", last_name, first_name, middle_name, &year) == 4) {
+        if (year > 1980) {
+            fprintf(output_file, "%s %s %s %d\n", last_name, first_name, middle_name, year);
+        }
+    }
+
+    fclose(input_file);
+    fclose(output_file);
+
     return 0;
 }
